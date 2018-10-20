7ff7f4a1ec78147e9937ab421bc8fe1319fe62ae
Ivan Nečas <inecas@redhat.com>
2018-10-20 22:33:46 +0200

    Add test cases about base branch and conflicts

--- cli/test/munihub_test.rb
+++ cli/test/munihub_test.rb
@@ -15,7 +15,7 @@ describe 'munihub.rb pull-request' do
         create_a_branch
         add_change
         commit_changes
-        run_munihub
+        run_munihub(editor_mode: 'sign')
         munihub_message.must_equal <<EOF
 My awesome patch
 
@@ -33,7 +33,7 @@ EOF
         add_change
         commit_changes('Second change', nil)
         add_change
-        run_munihub
+        run_munihub(editor_mode: 'sign')
         munihub_message.must_equal <<EOF
 # First change
 #
@@ -53,7 +53,7 @@ EOF
         add_change
         commit_changes('Second change', nil)
         add_change
-        message = run_munihub('leave')
+        message = run_munihub
         munihub_message.must_equal <<EOF
 # First change
 #
@@ -63,6 +63,44 @@ EOF
 EOF
         $?.exitstatus.wont_equal 0
         message.must_match(/empty message/)
+      end
+    end
+
+    describe 'when there is a conflict with the base branch' do
+      it 'exist with non-zero code and appropriate error message' do
+        create_a_branch('mybranch')
+
+        # simulate conflict in base branch
+        checkout('master')
+        add_change('change in master')
+        commit_changes('Another change in master')
+
+        checkout('mybranch')
+        add_change
+        commit_changes
+
+        message = run_munihub
+        $?.exitstatus.wont_equal 0
+        message.must_match(/conflict/)
+      end
+    end
+
+    describe 'when there is an alternative base branch specified' do
+      it 'uses the base branch' do
+        create_a_branch('new_base')
+        add_change
+        commit_changes
+
+        create_a_branch('mybranch')
+        add_change
+        commit_changes
+
+        puts run_munihub(args: ['-b', 'new_base'])
+        munihub_message.must_equal <<EOF
+My awesome patch
+
+I can not believe this repo was without it
+EOF
       end
     end
   end
--- cli/test/test_helper.rb
+++ cli/test/test_helper.rb
@@ -28,12 +28,16 @@ def clean_git_repo(dir)
   system('git', 'clean', '-f', dir)
 end
 
-def create_a_branch
-  system('git checkout -b mybranch')
+def create_a_branch(branch = 'mybranch')
+  system("git checkout -b #{branch}")
 end
 
-def add_change
-  File.open('README.md', 'a') { |f| f.puts 'new_line' }
+def checkout(branch = 'mybranch')
+  system("git checkout #{branch}")
+end
+
+def add_change(line = 'new_line')
+  File.open('README.md', 'a') { |f| f.puts line }
 end
 
 def commit_changes(subject = "My awesome patch", body = "I can not believe this repo was without it")
@@ -42,9 +46,9 @@ def commit_changes(subject = "My awesome patch", body = "I can not believe this 
   system("git commit -m '#{message}'")
 end
 
-def run_munihub(editor_mode = 'sign')
+def run_munihub(args: [], editor_mode: 'leave')
   fake_editor_env = { 'EDITOR' => "#{FAKE_EDITOR} #{editor_mode}" }
-  IO.popen(fake_editor_env, [MUNIHUB_PATH, 'pull-request'], :err=>[:child, :out]) { |io| io.read }
+  IO.popen(fake_editor_env, [MUNIHUB_PATH, 'pull-request'].concat(args), :err=>[:child, :out]) { |io| io.read }
 end
 
 def munihub_message
