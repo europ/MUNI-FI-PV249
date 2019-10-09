3cb28a34566b273c3745c29f62dc9d26d7131075
Adrián Tóth <adricko28@gmail.com>
2019-10-09 20:13:33 +0200

    Added: rspec

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module MiniHub
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.test_framework = :rspec
   end
 end
