206a83c26a8bfb79ecb7bfd44dd7f2394d2c5a0d
Adrián Tóth <adricko28@gmail.com>
2019-10-10 10:06:16 +0200

    Added: User - has_secure_password

--- proj1/app/models/user.rb
+++ proj1/app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true, format: { with: /\A[a-z]+\z/, message: "User name must be '\\A[a-z]+\\z'!" }
 
+  has_secure_password
+
   has_many :repositories
   has_many :issues
 end
