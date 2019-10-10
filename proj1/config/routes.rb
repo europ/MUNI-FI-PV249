80d2b24c0c721b02bc772728fa79fed6d895e7bb
Adrián Tóth <adricko28@gmail.com>
2019-10-10 20:24:51 +0200

    Added: routes - login/logout

--- proj1/config/routes.rb
+++ proj1/config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  resources :sessions
-  root to: 'repositories#index'
-  resources :issues
-  resources :repositories
+
   resources :users
+  resources :issues
+  #resources :sessions
+  resources :repositories
+
+  root to: 'repositories#index'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
