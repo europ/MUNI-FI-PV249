5568a72b5ebf7798ba04f0c8806fd0afb7f4320c
Adrián Tóth <adricko28@gmail.com>
2019-10-12 21:41:14 +0200

    Fix: exception handling for logout

--- proj1/app/controllers/session_controller.rb
+++ proj1/app/controllers/session_controller.rb
@@ -3,18 +3,26 @@ class SessionController < ApplicationController
   end
 
   def create
-  	@user = User.find_by(name: params[:session][:name])
+    @user = User.find_by(name: params[:session][:name])
     if @user && @user.authenticate(params[:session][:password])
       self.current_user = @user
       redirect_to root_path
     else
-      flash.now[:error] = "Failed to log in"
+      flash.now[:error] = "Failed to log in!"
       render :new
     end
   end
 
   def destroy
-  	self.current_user = nil
-    redirect_to root_path
+    if self.current_user == nil
+      redirect_to login_path
+    else
+      self.current_user = nil
+      begin
+        redirect_to login_path
+        flash[:success] = "Successfully logged out."
+      rescue AbstractController::DoubleRenderError
+      end
+    end
   end
 end
