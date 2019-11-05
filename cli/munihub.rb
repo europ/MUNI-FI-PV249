0d7eab6f15e7f02f732a28306fb394fc3ac691bb
Adrián Tóth <adricko28@gmail.com>
2019-11-05 14:26:54 +0100

    editor

--- cli/munihub.rb
+++ cli/munihub.rb
@@ -1,3 +1,44 @@
 #!/usr/bin/env ruby
 
-File.write('.git/MUNIHUB_MESSAGE', 'not implemented yet')
+require 'tempfile'
+
+class Editor
+  def initialize
+    begin
+      @editor = ENV.fetch('EDITOR')
+    rescue KeyError
+      raise Exception.new("Environment variable 'EDITOR' is not set!")
+    end
+  end
+
+  def load(text)
+    file = Tempfile.new
+    begin
+      write(file, text)
+      retval = system("#{@editor} #{file.path}")
+      if retval == true
+        data = read(file)
+      else
+        raise Exception.new("Editor error!")
+      end
+    ensure
+      file.close
+      file.unlink
+    end
+  end
+
+  def write(file, string)
+    file.write(string)
+    file.rewind
+  end
+
+  def read(file)
+    file.rewind
+    file.read
+  end
+end
+
+
+x = Editor.new
+t = x.load("hello")
+puts t
