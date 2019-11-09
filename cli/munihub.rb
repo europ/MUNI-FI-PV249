797b217d8597a465ee7535d21b13090d856ea020
Adrián Tóth <adricko28@gmail.com>
2019-11-09 14:52:17 +0100

    argparse

--- cli/munihub.rb
+++ cli/munihub.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+require 'optparse'
 require 'tempfile'
 
 class Editor
@@ -7,7 +8,7 @@ class Editor
     begin
       @editor = ENV.fetch('EDITOR')
     rescue KeyError
-      raise Exception.new("Environment variable 'EDITOR' is not set!")
+      raise Exception.new('Environment variable \'EDITOR\' is not set!')
     end
   end
 
@@ -19,7 +20,7 @@ class Editor
       if retval == true
         data = read(file)
       else
-        raise Exception.new("Editor error!")
+        raise Exception.new('Editor error!')
       end
     ensure
       file.close
@@ -38,7 +39,48 @@ class Editor
   end
 end
 
+class Options
+  def self.parse
+    if ARGV.first != 'pull-request'
+      error
+    else
+      ARGV.shift
+      if ARGV.length == 2 # -b base_branch
+        if ARGV.first == '-b'
+          ARGV.shift
+          return ARGV.first
+        else
+          error
+        end
+      elsif ARGV.length == 0 # no option
+        return 'master'
+      else
+        error
+      end
+    end
+  end
 
+  def self.error
+    help
+    raise Exception.new('Argument error!')
+  end
+
+  def self.help
+    print \
+    "Usage: munihub.rb pull-request [-b base_branch]\n" \
+    "    pull-request   ... mandatory ... action type\n" \
+    "    -b base_branch ... optional  ... targeted branch ('master' used as default)\n" \
+    "\n"
+  end
+end
+########################################################################
+
+
+branch = Options.parse
+puts "OK"
+
+=begin
 x = Editor.new
 t = x.load("hello")
 puts t
+=end
